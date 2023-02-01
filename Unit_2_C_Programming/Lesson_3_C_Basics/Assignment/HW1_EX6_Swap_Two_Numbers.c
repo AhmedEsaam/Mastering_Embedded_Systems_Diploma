6e4a5401977c697c40a2113e83a1320176837119
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-02-01 18:10:05 +0200

    HW1 - EX6

---
+++ Unit_2_C_Programming/Lesson_3_C_Basics/Assignment/HW1_EX6_Swap_Two_Numbers.c
@@ -0,0 +1,37 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Name      	: HW1_EX1_Print_a_Sentence.c
+ *
+ * Created on	: Feb 1, 2023
+ * Author    	: Ahmed Essam El-Mogy
+ * Description	: Unit 2. C Programming >> Lesson 3. C Basics >> Assignment
+ *  	          >> Homework 1 >> EX6 - Swap Two Numbers
+ * C Standard	: ANSI C
+ * ----------------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	float a, b, temp;
+
+	printf("Enter value of a: ");
+	fflush(stdout);
+	scanf("%f", &a);
+
+	printf("Enter value of b: ");
+	fflush(stdout);
+	scanf("%f", &b);
+
+	temp = b;
+	b = a;
+	a = temp;
+
+	printf("\nAfter swapping, value of a = %f\n", a);
+	printf("After swapping, value of b = %f\n", b);
+
+	return 0;
+}
+
+
