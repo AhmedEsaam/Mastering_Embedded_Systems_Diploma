408e9544b6c8d02e075dd526574fb6507adf170a
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-02-01 18:09:25 +0200

    HW1 - EX4

---
+++ Unit_2_C_Programming/Lesson_3_C_Basics/Assignment/HW1_EX4_Multiply_two_Floating_Point_Numbers.c
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Name      	: HW1_EX1_Print_a_Sentence.c
+ *
+ * Created on	: Feb 1, 2023
+ * Author    	: Ahmed Essam El-Mogy
+ * Description	: Unit 2. C Programming >> Lesson 3. C Basics >> Assignment
+ *  	          >> Homework 1 >> EX4 - Multiply two Floating Point Numbers
+ * C Standard	: ANSI C
+ * ----------------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	float num1, num2;
+	printf("Enter two numbers: \n");
+	fflush(stdin); fflush(stdout);
+	scanf("%f\n%f", &num1, &num2);
+	printf("Product: %f\n", num1 + num2);
+
+	return 0;
+}
+
+
