5f3b5d7e5f364e6f4fd26ac9ab7a2802bf2f3d95
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-02-01 15:53:43 +0200

    HW1 EX1

---
+++ Unit_2_C_Programming/Lesson_3_C_Basics/Assignment/HW1_EX1_Print_a_Sentence.c
@@ -0,0 +1,22 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Name      	: HW1_EX1_Print_a_Sentence.c
+ *
+ * Created on	: Feb 1, 2023
+ * Author    	: Ahmed Essam El-Mogy
+ * Description	: Unit 2. C Programming >> Lesson 3. C Basics >> Assignment
+ *  	          >> Homework 1 >> EX1 - Print a Sentence
+ * C Standard	: ANSI C
+ * ----------------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("C Programming\n");
+
+	return 0;
+}
+
+
