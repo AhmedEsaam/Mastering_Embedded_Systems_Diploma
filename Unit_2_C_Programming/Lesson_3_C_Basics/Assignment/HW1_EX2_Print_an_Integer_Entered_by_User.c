4c462396d6052e2d87eb8bb5632c53d5df15e265
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-02-01 17:56:47 +0200

    HW1 - EX2

---
+++ Unit_2_C_Programming/Lesson_3_C_Basics/Assignment/HW1_EX2_Print_an_Integer_Entered_by_User.c
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Name      	: HW1_EX1_Print_a_Sentence.c
+ *
+ * Created on	: Feb 1, 2023
+ * Author    	: Ahmed Essam El-Mogy
+ * Description	: Unit 2. C Programming >> Lesson 3. C Basics >> Assignment
+ *  	          >> Homework 1 >> EX3 - Print an Integer Entered by User
+ * C Standard	: ANSI C
+ * ----------------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int num;
+	printf("Enter an integer: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d", &num);
+	printf("You entered: %d\n", num);
+
+	return 0;
+}
+
+
