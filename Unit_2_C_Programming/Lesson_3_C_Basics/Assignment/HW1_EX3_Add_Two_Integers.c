62375f9f038e05944d58d99b864a3601cb94f547
Ahmed Essam <ahmed.e.mogy@gmail.com>
2023-02-01 18:08:47 +0200

    HW1 EX3

---
+++ Unit_2_C_Programming/Lesson_3_C_Basics/Assignment/HW1_EX3_Add_Two_Integers.c
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------------------------
+ * Name      	: HW1_EX1_Print_a_Sentence.c
+ *
+ * Created on	: Feb 1, 2023
+ * Author    	: Ahmed Essam El-Mogy
+ * Description	: Unit 2. C Programming >> Lesson 3. C Basics >> Assignment
+ *  	          >> Homework 1 >> EX3 - Add Two Integers
+ * C Standard	: ANSI C
+ * ----------------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	printf("Enter two integers: \n");
+	fflush(stdin); fflush(stdout);
+	scanf("%d %d", &num1, &num2);
+	printf("Sum: %d\n", num1 + num2);
+
+	return 0;
+}
+
+
